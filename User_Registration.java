b359776506e30e6da8aee9336af603f61e1dbcec
rushitayade71 <rushitayade71@gmail.com>
2021-04-07 12:45:11 +0530

    Added: User first name

--- User_Registration.java
+++ User_Registration.java
@@ -1,5 +1,34 @@
+import java.util.Scanner;
+import java.util.regex.*;
+
 public class User_Registration {
+
+	static String fName;
+
+	static String firstName(){
+		Scanner sca = new Scanner(System.in);
+		System.out.println("Enter your first name : ");
+		fName = sca.nextLine();
+		return fName;
+	}
+
 	public static void main(String args[]) {
-	System.out.println("Welcome to User Registration Regex Problem");
+		System.out.println("Welcome to User Registration Regex Problem");
+
+		Boolean firstName = Pattern.matches("^[A-Z]{1}[a-z]{2,}",User_Registration.firstName());
+
+		if(Boolean.TRUE.equals(firstName))
+		{
+			System.out.println("Correct format");
+		}
+		else
+
+		{
+			System.out.println("Incorrect format try   'R'ushi this format");
+		}
+
+		System.out.println("First Name: " + fName);
+
 	}
 }
+
