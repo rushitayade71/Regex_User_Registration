0ca30599bc34827d0d361cdccf411637a7067314
rushitayade71 <rushitayade71@gmail.com>
2021-04-07 16:17:52 +0530

    Added: User Email Id

--- User_Registration.java
+++ User_Registration.java
@@ -1,3 +1,4 @@
+import java.util.regex.Matcher;
 import java.util.Scanner;
 import java.util.regex.*;
 
@@ -5,6 +6,7 @@ public class User_Registration {
 
 	static String fName;
 	static String lName;
+	static String email;
 
 	static String firstName(){
 		Scanner sca1 = new Scanner(System.in);
@@ -13,12 +15,20 @@ public class User_Registration {
 		return fName;
 	}
 
- static String lastName(){
+	static String lastName(){
                 Scanner sca2 = new Scanner(System.in);
                 System.out.println("Enter your last name : ");
                 lName = sca2.nextLine();
                 return lName;
         }
+
+        static String emailId(){
+                Scanner sca3 = new Scanner(System.in);
+                System.out.println("Enter your E-Mail Id : ");
+                email = sca3.nextLine();
+                return email;
+        }
+
 
 
 	public static void main(String args[]) {
@@ -26,6 +36,7 @@ public class User_Registration {
 
 		Boolean firstName = Pattern.matches("^[A-Z]{1}[a-z]{2,}",User_Registration.firstName());
                 Boolean lastName = Pattern.matches("^[A-Z]{1}[a-z]{2,}",User_Registration.lastName());
+                Boolean emailId =  Pattern.matches("^[a][b][c][.][a-z]{3}[@][b][l][.][c][o][.][a-z]{2}",User_Registration.emailId());
 
 		if(firstName == true)
 		{
@@ -33,22 +44,32 @@ public class User_Registration {
                         System.out.println("Correct format");
 		}
 		else
-
 		{
 			System.out.println("Incorrect format try   'R'ushi this format");
 		}
+
 		if(lastName == true)
                 {
                 System.out.println("Last Name: " + lName);
                         System.out.println("Correct format");
 		}
-
-		 else
-
+		else
                 {
                         System.out.println("Incorrect format try   'T'ayade this format");
                 }
 
+		if(emailId == true)
+                {
+                System.out.println("E-Mail Id : " + email);
+                        System.out.println("Correct format");
+                }
+                 else
+                {
+                        System.out.println("Incorrect format!");
+                }
+
+
 	}
 }
+
 
