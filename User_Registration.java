bebc33e17b4b14c25885df68db7eb4e355b5b0d3
rushitayade71 <rushitayade71@gmail.com>
2021-04-07 13:41:59 +0530

    Added: User last name

--- User_Registration.java
+++ User_Registration.java
@@ -4,30 +4,50 @@ import java.util.regex.*;
 public class User_Registration {
 
 	static String fName;
+	static String lName;
 
 	static String firstName(){
-		Scanner sca = new Scanner(System.in);
+		Scanner sca1 = new Scanner(System.in);
 		System.out.println("Enter your first name : ");
-		fName = sca.nextLine();
+		fName = sca1.nextLine();
 		return fName;
 	}
+
+ static String lastName(){
+                Scanner sca2 = new Scanner(System.in);
+                System.out.println("Enter your last name : ");
+                lName = sca2.nextLine();
+                return lName;
+        }
+
 
 	public static void main(String args[]) {
 		System.out.println("Welcome to User Registration Regex Problem");
 
 		Boolean firstName = Pattern.matches("^[A-Z]{1}[a-z]{2,}",User_Registration.firstName());
+                Boolean lastName = Pattern.matches("^[A-Z]{1}[a-z]{2,}",User_Registration.lastName());
 
-		if(Boolean.TRUE.equals(firstName))
+		if(firstName == true)
 		{
-			System.out.println("Correct format");
+                System.out.println("First Name: " + fName);
+                        System.out.println("Correct format");
 		}
 		else
 
 		{
 			System.out.println("Incorrect format try   'R'ushi this format");
 		}
+		if(lastName == true)
+                {
+                System.out.println("Last Name: " + lName);
+                        System.out.println("Correct format");
+		}
 
-		System.out.println("First Name: " + fName);
+		 else
+
+                {
+                        System.out.println("Incorrect format try   'T'ayade this format");
+                }
 
 	}
 }
